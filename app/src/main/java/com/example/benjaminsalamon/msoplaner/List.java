d2e72db2439939a5f6ce802669ddbb0cdf3d47e7
Johannes Randerath <joranderathsg@gmail.com>
2017-02-05 16:30:30 +0100

    - added Class Structure    - added Day Class       - added Instance variables:          - index: int, lessons: List<Lesson>, name: String       - added methods          - added constructor Day(pIndex: int, pName: String)          - added addLesson(pSubject: Subject, pStartH: int, pStartM: int, pEndH: int, pEndM: int): void          - added bubbleSortLessonsByTime(): void    - added Lesson Class      - added instance variables         - endHour: int, endMin: int, sbjct: Subject, startHour: int, startMin: int         - added methods            - added constructor Lesson(pSbjct: Subject, pStartH: int, pStartM: int, pEndH: int, pEndM: int)            - added getEndHour() : int            - added getEndMin() : int            - added getStartHour() : int            - added getStartMin() : int            - added getSubject() : Subject            - added setEndHour(int pValue) : void            - added setEndMin(int pValue) : void            - added setSbjct(Subject pValue) : void            - added setStartHour(int pValue) : void            - added setStartMin(int pValue) : void    - added List<T> Class       - added private class ListNode          - added instance variables             - content: T, next: List.ListNode          - added methods             - added constructor ListNode(pContent: T, pNext List.ListNode)             - added getContent() : T             - added getNext() : List.ListNode             - added hasNext() : boolean             - added setContent(pContent: T) : void             - added setNext(n: List.ListNode) : void       (Class List:)       - added instance variables         - current: List.ListNode         - first: List.ListNode         - last: List.ListNode      - added methods         - added constructor List()         - added append(pContent: T) : void         - added concat(second: List<T>): void         - added getContent() : T         - added getPrevious() : List.ListNode         - added getSize() : int         - added hasAccess() : boolean         - added insert(pContent: T): void         - added isEmpty() : boolean         - added next() : void         - added previous() : void         - added remove() : void         - added setContent(pContent: T) : void         - added toFirst() : void         - added toLast() : void    - added Stack Class       - for documentation please see javaDoc    - added Subject Class       - added instance variables          - abbreviation : String, color : Color, id: int, name: String, place: String, teacher: String, type: String       - added methods          - added constructor Subject(int pId, java.lang.String pName, java.lang.String pAbbreviation, java.lang.String pPlace, java.lang.String pType, java.lang.String pTeacher, android.graphics.Color pColor)          - added getAbbreviation() : String          - added getColor() : Color          - added getId() : int          - added getName() : String          - added getPlace() : String          - added getTeacher() : String          - added getType() : String          - added setAbbreviation(pValue: String) : void          - added setColor(pValue: Color) : void          - added setName(pValue: String) : void          - added setPlace(pValue: String) : void          - added setTeacher(pValue: String) : void          - added setTeacher(pValue: String) : void    - added Application Logic      - added instance variables         - subjects : Stack<Subject>      - added methods         - added constructor Logic()         - added addSubject(java.lang.String pName, java.lang.String pAbbreviation, java.lang.String pPlace, java.lang.String pType, java.lang.String pTeacher, android.graphics.Color color) : void

---
+++ app/src/main/java/com/example/benjaminsalamon/msoplaner/List.java
@@ -0,0 +1,157 @@
+package com.example.benjaminsalamon.msoplaner;
+
+/**
+ * Created by johannes on 02/02/2017.
+ */
+
+class List<T> {
+
+    private class ListNode {
+
+        private T content;
+        private ListNode next;
+
+        protected ListNode(T pContent, ListNode pNext) {
+
+            content = pContent;
+            next = pNext;
+
+        }
+
+        protected void setContent(T pContent) {
+            content = pContent;
+        }
+
+        protected T getContent() {
+            return content;
+        }
+
+        protected void setNext(ListNode n) {
+            next = n;
+        }
+
+        protected ListNode getNext() {
+            return next;
+        }
+
+        protected boolean hasNext() {
+            return next != null;
+        }
+
+    }
+
+    private ListNode first;
+    private ListNode current;
+    private ListNode last;
+
+    public List(){
+        first = null;
+        current = null;
+        last = null;
+    }
+
+    public void append(T pContent) {
+
+        if(pContent != null) {
+            ListNode n = new ListNode(pContent, null);
+            last.setNext(n);
+            last = n;
+            if(isEmpty()) first = n;
+        }
+
+    }
+
+    public void concat(List<T> second) {
+        if(!second.isEmpty()) {
+            second.toFirst();
+            append(second.getContent());
+        }
+    }
+
+    public T getContent() {
+        if(hasAccess()) {
+            return current.getContent();
+        }else {
+            return null;
+        }
+    }
+
+    public boolean hasAccess() {
+        return current != null;
+    }
+
+    public void insert(T pContent) {
+        if(pContent != null) {
+            if (hasAccess()) {
+                ListNode n = new ListNode(pContent, current);
+                if (first == current) first = n;
+                else {
+                    getPrevious().setNext(n);
+                }
+            }else if(isEmpty()){
+                append(pContent);
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void next() {
+        if(hasAccess()) current = current.getNext();
+        else this.toFirst();
+    }
+
+    public void previous() {
+        current = getPrevious();
+    }
+
+    public void remove() {
+        if(hasAccess()) {
+            if(first == current) first = current.getNext();
+            else {
+                if(last == current) last = getPrevious();
+                else getPrevious().setNext(current.getNext());
+            }
+        }
+    }
+
+    public void setContent(T pContent) {
+
+        if(hasAccess() && pContent != null) {
+            current.setContent(pContent);
+        }
+
+    }
+
+    public void toFirst() {
+        current = first;
+    }
+
+    public void toLast() {
+        current = last;
+    }
+
+    private ListNode getPrevious() {
+
+        if(hasAccess() && current != first) {
+            ListNode n = first;
+            while(n.getNext() != current) n = n.getNext();
+            return n;
+        }
+        return null;
+    }
+
+    public int getSize() {
+
+        int i = 0;
+        this.toFirst();
+        while (hasAccess()) {
+            i++;
+            this.next();
+        }
+        return i;
+    }
+
+}
---
+++ app/src/main/java/com/example/benjaminsalamon/msoplaner/Logic.java
@@ -0,0 +1,48 @@
+package com.example.benjaminsalamon.msoplaner;
+
+import android.graphics.Color;
+
+/**
+ * Created by johannes on 02/02/2017.
+ */
+
+class Logic {
+
+    private Stack<Subject> subjects;
+
+    public Logic() {
+
+        subjects = new Stack<Subject>();
+
+    }
+
+    public void addSubject(String pName, String pAbbreviation, String pPlace, String pType, String pTeacher, Color color) {
+
+        int id;
+        String name, abbreviation, place, type, teacher;
+
+        if(!subjects.isEmpty()) id = subjects.top().getId() + 1;
+        else id = 0;
+
+        if(pName.equals("") || pName.equals(" ")) name = null;
+        else name = pName;
+
+        if(pAbbreviation.equals("") || pAbbreviation.equals(" ")) abbreviation = null;
+        else abbreviation = pAbbreviation;
+
+        if(pPlace.equals("") || pPlace.equals(" ")) place = null;
+        else place = pPlace;
+
+        if(pType.equals("") || pType.equals(" ")) type = null;
+        else type = pType;
+
+        if(pTeacher.equals("") || pTeacher.equals(" ")) teacher = null;
+        else teacher = pTeacher;
+
+        Subject s = new Subject(id, name, abbreviation, place, type, teacher, color);
+        subjects.push(s);
+    }
+
+
+
+}
