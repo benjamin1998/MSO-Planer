d55dda24667e6974bbf9817161766c25e0c06176
benjamin1998 <benjamin.salamon@outlook.com>
2017-02-05 19:57:05 +0100

    fragment

--- app/src/main/java/com/example/benjaminsalamon/msoplaner/MainActivity.java
+++ app/src/main/java/com/example/benjaminsalamon/msoplaner/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.benjaminsalamon.msoplaner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -39,8 +39,6 @@ public class MainActivity extends AppCompatActivity
         buttonRight = (Button) findViewById(R.id.buttonRight); //initializing button variable as View
         buttonLeft = (Button) findViewById(R.id.buttonLeft);
         buttonRight.setText("<"); //set button text
-
-
     }
 
     @Override
